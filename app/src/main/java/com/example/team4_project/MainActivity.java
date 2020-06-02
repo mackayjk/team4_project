676ad024537cd5843d8d615869a5c5e216a8d3f5
mackaykillian <mackay.killian@gmail.com>
2020-06-01 19:17:10 -0600

    Testing first change to the file

--- app/src/main/java/com/example/team4_project/MainActivity.java
+++ app/src/main/java/com/example/team4_project/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*Testing Comment*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

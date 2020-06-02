f416740af5dcdd8215b217275d269954d32ddc95
mackaykillian <mackay.killian@gmail.com>
2020-06-01 19:20:46 -0600

    Testing merge conflict

--- app/src/main/java/com/example/team4_project/MainActivity.java
+++ app/src/main/java/com/example/team4_project/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     /*Testing Comment*/
-
+    /*change made by MacKay*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

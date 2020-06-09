4d3481470ee0f2f1a6678faa9f330f49005c739c
mountain <Fiveoat@gmail.com>
2020-06-08 19:38:23 -0600

    wrote test

--- app/src/test/java/com/example/team4_project/ExampleUnitTest.java
+++ app/src/test/java/com/example/team4_project/ExampleUnitTest.java
@@ -1,17 +1,26 @@
 package com.example.team4_project;
 
+import android.widget.Button;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testOneRandomRestaurantReturnedWhenButtonPressed(){
+        assertEquals(getRandomRestaurant().length, 1);
     }
+
+    @Test
+    public void testGoogleAPIConnectsProperly(){
+        assertTrue(connectToAPI());
+    }
+
+    @Test
+    public void testDatabaseReturnsRestaurantTypeObject(){
+        assertTrue(getRestaurant() instanceof Restaurant);
+    }
+
 }

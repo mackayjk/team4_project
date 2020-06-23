2dcb8c0e683777e13e1c34053f8527ea96a5fd4f
mackaykillian <mackay.killian@gmail.com>
2020-06-22 19:23:50 -0600

    changes to Main activity, add show restaurant data

--- app/src/main/java/com/example/team4_project/MainActivity.java
+++ app/src/main/java/com/example/team4_project/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.team4_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*Testing Comment*/
-    /*change made by MacKay*/
+    private static final String TAG = "MyActivity";
+    PlacesClient placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //This method will initialize the places client.
+    public void initializePlaces() {
+        Places.initialize(getApplicationContext(), "AIzaSyB2Ul315SoD2GRH-xCMvpYUQ3sOlsx7u-Q");
+        placesClient = Places.createClient(this);
+    }
+
+
+    public void loadRestaurant(View view) throws InterruptedException {
+        initializePlaces();
+        String placeId = "ChIJf83U3auQTYcRcsZgTfnaQOg";
+        RestaurantLoader restaurantLoader = new RestaurantLoader(placesClient);
+        Restaurant r = restaurantLoader.getRestaurant(placeId, this);
+        //Toast.makeText(this, "testing the toast message." + r.getRestaurantName(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void showData(Restaurant r)
+    {
+        Toast.makeText(this, "testing the toast message." + r.getRestaurantName(), Toast.LENGTH_SHORT).show();
     }
 }

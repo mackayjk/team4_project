f8e6ba609ca1743a6ef96969bf4292a719cfef36
mackaykillian <mackay.killian@gmail.com>
2020-07-11 14:15:17 -0600

    cleaned up files

--- app/src/main/java/com/example/team4_project/SignIn.java
+++ app/src/main/java/com/example/team4_project/SignIn.java
@@ -1,4 +1,14 @@
 package com.example.team4_project;
 
-public class SignIn {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
 }
--- app/src/main/java/com/example/team4_project/SignUp.java
+++ app/src/main/java/com/example/team4_project/SignUp.java
@@ -1,4 +1,14 @@
 package com.example.team4_project;
 
-public class SignUp {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
 }
